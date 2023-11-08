73d74b8ee2516500f2108fb55fcc1204d1df7469
Philippe ENTZMANN <philippe@arundo.tech>
2023-11-08 10:35:45 +0100

    fix: wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,5 +19,18 @@ export default defineNuxtConfig({
     // Remove it if you don't use Plausible analytics
     // https://github.com/nuxt-modules/plausible
     '@nuxtjs/plausible'
-  ]
+  ],
+
+
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        // see https://github.com/nuxt-themes/docus/issues/996
+    
+        // Error:  Nuxt Build Error: [vite]: Rollup failed to resolve import "@supabase/auth-ui-shared" from "/home/runner/work/pp-mycompany/pp-mycompany/node_modules/@arundo-tech/paxpar-widgets/pages/login.vue?vue&type=script&setup=true&lang.ts".
+        verbatimModuleSyntax: false
+      },
+    },
+  },
+
 })
