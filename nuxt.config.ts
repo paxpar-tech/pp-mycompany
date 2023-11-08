17776396ce79e85fb4d0d24dc95af4f1feba4f99
Philippe ENTZMANN <philippe@arundo.tech>
2023-11-08 14:29:36 +0100

    fix: wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,15 +22,30 @@ export default defineNuxtConfig({
   ],
 
 
-  typescript: {
-    tsConfig: {
-      compilerOptions: {
-        // see https://github.com/nuxt-themes/docus/issues/996
-    
-        // Error:  Nuxt Build Error: [vite]: Rollup failed to resolve import "@supabase/auth-ui-shared" from "/home/runner/work/pp-mycompany/pp-mycompany/node_modules/@arundo-tech/paxpar-widgets/pages/login.vue?vue&type=script&setup=true&lang.ts".
-        verbatimModuleSyntax: false
-      },
+  // typescript: {
+  //   tsConfig: {
+  //     compilerOptions: {
+  //       // see https://github.com/nuxt-themes/docus/issues/996
+  //   
+  //       // Error:  Nuxt Build Error: [vite]: Rollup failed to resolve import "@supabase/auth-ui-shared" from "/home/runner/work/pp-mycompany/pp-mycompany/node_modules/@arundo-tech/paxpar-widgets/pages/login.vue?vue&type=script&setup=true&lang.ts".
+  //       verbatimModuleSyntax: false
+  //     },
+  //   },
+  // },
+
+  vite: {
+    vue: {
+      script: {
+        // opt-in, see https://github.com/vuejs/rfcs/discussions/503
+        // and https://github.com/nuxt/nuxt/issues/20881
+        defineModel: true,
+        // opt-in, see https://github.com/vuejs/rfcs/discussions/503
+        // and https://github.com/nuxt/nuxt/issues/20881
+        propsDestructure: true
+      }
     },
+
   },
+
 
 })
